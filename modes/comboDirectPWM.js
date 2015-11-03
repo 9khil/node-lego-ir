dad80b24dedbdea25db6dca01d1e1af50f4b83de
Nikhil André Luthra <nikhil.a.luthra@gmail.com>
2015-11-03 22:35:01 +0100

    updating documentation, adding another mode

---
+++ modes/comboDirectPWM.js
@@ -0,0 +1,25 @@
+var mode = {
+	code: '0002',
+	outputs: ['outputA', 'outputB'],
+	moves: {
+		float: '0000',
+		forward1: '0001', 
+		forward2: '0010',
+		forward3: '0011',
+		forward4: '0100',
+		forward5: '0101',
+		forward6: '0110',
+		forward7: '0111',
+		brake: '1000',
+		backward7: '1001',
+		backward6: '1010',
+		backward5: '1011',
+		backward4: '1100',
+		backward3: '1101',
+		backward2: '1110',
+		backward1: '1111'
+
+	}
+};
+
+module.exports = mode;
